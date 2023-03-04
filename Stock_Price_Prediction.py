a09cc3d5ce236ed930816946ff60454286a20bbb
Muhammad Hur Khan <122796904+MuhammadHurKhan@users.noreply.github.com>
2023-03-04 13:16:12 +0500

    Update Stock_Price_Prediction.py

--- Stock_Price_Prediction.py
+++ Stock_Price_Prediction.py
@@ -3,64 +3,64 @@
 
 # In[31]:
 
-
 import yfinance as yf
+import streamlit as st
 import numpy as np
 import pandas as pd
-import streamlit as st
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
-from plotly.subplots import make_subplots
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from sklearn.preprocessing import MinMaxScaler
 import plotly.graph_objs as go
 
-# Function to create a LSTM model
-def create_model(train_data):
-    model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape=(train_data.shape[1], 1)))
-    model.add(LSTM(50, return_sequences=False))
-    model.add(Dense(25))
-    model.add(Dense(1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    return model
+# Set page title and favicon
+st.set_page_config(page_title='Stock Price Prediction', page_icon=':money_with_wings:')
 
-# Function to preprocess the data
-def preprocess_data(data):
+# Define function to preprocess the data
+def preprocess_data(stock_data):
+    data = pd.DataFrame(stock_data)
     data['Date'] = pd.to_datetime(data.Date, format='%Y-%m-%d')
     data.index = data['Date']
-    data.drop(['Date'], axis=1, inplace=True)
-    data = data.sort_index(ascending=True, axis=0)
-    return data
-
-# Function to create the training and testing data sets
-def create_train_test_data(data, train_data_size):
-    train_data = data[0:train_data_size, :]
-    x_train = []
-    y_train = []
-    for i in range(60, train_data_size):
-        x_train.append(train_data[i-60:i, 0])
-        y_train.append(train_data[i, 0])
+    data.drop('Date', axis=1, inplace=True)
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(data)
+    x_train, y_train = [], []
+    for i in range(60,len(data)):
+        x_train.append(scaled_data[i-60:i,0])
+        y_train.append(scaled_data[i,0])
     x_train, y_train = np.array(x_train), np.array(y_train)
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    return x_train, y_train
+    return x_train, y_train, scaler
 
-# Function to make predictions
-def make_predictions(model, x_test):
-    predictions = model.predict(x_test)
-    predictions = scaler.inverse_transform(predictions)
-    return predictions
+# Define function to create the LSTM model
+def create_model(x_train):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+    model.add(LSTM(units=50))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
 
-# Get the stock data
-ticker = st.text_input("Enter stock ticker symbol (e.g. AAPL for Apple): ")
-stock_data = yf.download(ticker, period="max")
+# Define function to make the prediction
+def make_prediction(model, x_train, scaler, days):
+    prediction_data = x_train[-1]
+    predicted_prices = []
+    for _ in range(days):
+        prediction = model.predict(prediction_data.reshape(1,60,1))
+        predicted_price = scaler.inverse_transform(prediction)[0][0]
+        predicted_prices.append(predicted_price)
+        prediction_data = np.append(prediction_data[1:], prediction[0])
+    return predicted_prices
+
+# Set up the app layout
+st.title("Stock Price Prediction")
+st.sidebar.header("Enter the Stock Ticker")
+ticker = st.sidebar.text_input("Ticker", "AAPL")
+
+# Fetch the data
+stock_data = yf.download(ticker, start="2010-01-01", end="2022-01-01")
 
 # Preprocess the data
-data = preprocess_data(stock_data)
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data)
-
-# Create the training and testing data sets
-train_data_size = int(len(scaled_data) * 0.8)
-x_train, y_train = create_train_test_data(scaled_data, train_data_size)
+x_train, y_train, scaler = preprocess_data(stock_data)
 
 # Create the LSTM model
 model = create_model(x_train)
@@ -68,25 +68,41 @@ model = create_model(x_train)
 # Train the model
 model.fit(x_train, y_train, epochs=1, batch_size=1, verbose=2)
 
-# Make predictions
-test_data = scaled_data[train_data_size - 60: , :]
-x_test = []
-for i in range(60, len(test_data)):
-    x_test.append(test_data[i-60:i, 0])
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-predictions = make_predictions(model, x_test)
+# Make the prediction
+days = 7
+predicted_prices = make_prediction(model, x_train, scaler, days)
 
-# Show the prediction graph
-fig = make_subplots(rows=1, cols=1)
-fig.add_trace(go.Scatter(x=data.index[train_data_size:], y=data['Close'][train_data_size:], mode='lines', name='Actual'))
-fig.add_trace(go.Scatter(x=data.index[train_data_size:], y=predictions[:,0], mode='lines', name='Predicted'))
+# Create the plot
+data = stock_data.filter(['Close'])
+last_date = data.index[-1]
+date_range = pd.date_range(last_date, periods=days+1, freq='B')
+date_range = date_range[1:]
+predicted_data = pd.DataFrame(data=predicted_prices, index=date_range, columns=['Prediction'])
+data = pd.concat([data, predicted_data], axis=0)
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name='Actual Price'))
+fig.add_trace(go.Scatter(x=data.index, y=data['Prediction'], name='Predicted Price'))
+
+#Update the plot layout
 fig.update_layout(title=f"Stock Price Prediction for {ticker.upper()}",
 xaxis_title="Date",
-yaxis_title="Stock Price ($)",
-xaxis_rangeslider_visible=True)
-fig.add_trace(go.Scatter(x=future_dates, y=future_predictions, mode="lines", name="Future Predictions"))
+yaxis_title="Stock Price ($)")
+
+#Plot the predicted values
+fig.add_trace(go.Scatter(x=future_dates, y=predictions.flatten(),
+mode="lines",
+name="Predicted Price"))
+
+#Show the plot
 st.plotly_chart(fig)
+
+
+
+
+
+
+
 
 
 # In[ ]:
